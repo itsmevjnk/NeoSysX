a6c9e8a522026bbdeb8ada40681a3685c088ecd3
Vinh Nguyen <ngtv0404@gmail.com>
2026-07-19 12:17:24 +1000

    fix GDT bug

--- kernel/cpu/i386/src/gdt.c
+++ kernel/cpu/i386/src/gdt.c
@@ -2,10 +2,10 @@
 
 gdt_entry_t gdt_table[5] = {
     GDT_NULL,
-    GDT_4K_CSEG32(0, 0x10000000ULL, 1, 0), // 0x08 - ring 0 code segment
-    GDT_4K_DSEG32(0, 0x10000000ULL, 0), // 0x10 - ring 0 data segment
-    GDT_4K_CSEG32(0, 0x10000000ULL, 0, 3), // 0x18 - ring 3 data segment
-    GDT_4K_DSEG32(0, 0x10000000ULL, 3), // 0x20 - ring 3 data segment
+    GDT_4K_CSEG32(0, 0x100000000ULL, 1, 0), // 0x08 - ring 0 code segment
+    GDT_4K_DSEG32(0, 0x100000000ULL, 0), // 0x10 - ring 0 data segment
+    GDT_4K_CSEG32(0, 0x100000000ULL, 0, 3), // 0x18 - ring 3 data segment
+    GDT_4K_DSEG32(0, 0x100000000ULL, 3), // 0x20 - ring 3 data segment
 };
 
 gdt_descriptor_t gdt_desc = {
